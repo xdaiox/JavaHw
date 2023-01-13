0300b4dad05c5afd6e28c74445c3ba10d635df10
“xdaiox” <tttmmmmm@gmail.com>
2023-01-13 10:07:27 +0800

    add min function on MaxMin

--- src/com/xdaiox/hw3/MaxMin.java
+++ src/com/xdaiox/hw3/MaxMin.java
@@ -4,14 +4,23 @@ public class MaxMin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] data = {1,2,3,4,15,666,7,8,9,10};
+		int[] data = {1,1,4,4,15,666,7,8,9,10};
 		int x = data[0];
+		int y = x;
+		
 		for(int i = 0;i<9;i++) {
-			if(x < data[i+1]) {
+			if(x < data[i+1]) { //找出最大值
 				x = data[i+1];
 			}
 		}//i = 0;i<9;i++
-		System.out.println(x);
+		System.out.println("max: "+ x);
+		
+		for(int i = 0;i<9;i++) {
+			if(y > data[i+1]) { //找出最小值
+				y = data[i+1];
+			}
+		}//i = 0;i<9;i++
+		System.out.println("min: "+ y);
 	}
 
 }
