32aaa89d4752018d9694c81e5efb225ddcb1c858
“xdaiox” <tttmmmmm@gmail.com>
2023-01-19 16:22:17 +0800

    upddate hw5

---
+++ src/com/xdaiox/hw5/Shape.java
@@ -0,0 +1,9 @@
+package com.xdaiox.hw5;
+
+public interface Shape {
+	public double computeArea(double area);
+	public double computeCircumference(double x);
+}
+
+
+
---
+++ src/com/xdaiox/hw5/test2.java
@@ -0,0 +1,9 @@
+package com.xdaiox.hw5;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		Rectangle reg = new Rectangle();
+	}
+
+}
